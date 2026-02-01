99bdb19c979e9f2fa802834cf061bf790c92ecac
ilknurgeylan <ilknurgeylan@outlook.com>
2026-02-02 00:06:29 +0300

    CALISMA

---
+++ 9_3_Deneme/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _9_3_Deneme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 10 ile 20 arasındaki sayıları FOR ile yazma
+            for (int i=10;i<=20; i++)
+            {
+                Console.WriteLine(i);
+            }
+            #endregion
+            #region  10 ile 20 arasındaki sayıları WHİLE ile yazma
+            int a = 10;
+            while(a<=20)
+            {
+                Console.WriteLine(a);
+                a++;
+            }
+            #endregion
+        }
+    }
+}
